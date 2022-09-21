64ab5e70d41c21018851b070bb0ffda240f91055
Alejandro Quiceno Mejía <alejo.123.ks@gmail.com>
2022-09-21 01:21:31 -0500

    Movement done

--- EvolootFroggerProject/Assets/Scripts/PlayerMovement.cs
+++ EvolootFroggerProject/Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,9 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float MovementDuration=0.15f;
     [SerializeField] private float JumpPower;
-
+    [SerializeField] private float ShakePower=0.2f;
+    [SerializeField] private float ShakeRandomness = 90;
+    [SerializeField] private Ease JumpEase;
     void Start()
     {
         
@@ -19,16 +21,19 @@ public class PlayerMovement : MonoBehaviour
         {
             if (Input.GetAxis("Horizontal")!= 0 )
             {
-                Jump.Insert(0,transform.DOJump(new Vector3(transform.position.x + Mathf.Sign(Input.GetAxis("Horizontal")), transform.position.y, transform.position.z),JumpPower,1, MovementDuration, false));
+                Jump.Insert(0,transform.DOJump(new Vector3(transform.position.x + Mathf.Sign(Input.GetAxis("Horizontal")), transform.position.y, transform.position.z),JumpPower,1, MovementDuration, false)).SetEase(JumpEase);
+                Jump.Insert(0, transform.DORotate(new Vector3(0, 90 * Mathf.Sign(Input.GetAxis("Horizontal")), 0), MovementDuration));
+                Jump.Append(transform.DOShakeScale(0.15f,ShakePower*new Vector3(-1,1,-1),1,90,true));
             }
             else if(Input.GetAxis("Vertical") != 0 )
             {
-                Jump.Insert(0,transform.DOJump(new Vector3(transform.position.x , transform.position.y, transform.position.z+ Mathf.Sign(Input.GetAxis("Vertical"))),JumpPower,1, MovementDuration, false));
+                float rotationValue;
+                if (Mathf.Sign(Input.GetAxis("Vertical")) < 0) rotationValue = 180f;
+                else rotationValue = 0f;
+                Jump.Insert(0, transform.DORotate(new Vector3(0, rotationValue, 0), MovementDuration));
+                Jump.Insert(0,transform.DOJump(new Vector3(transform.position.x , transform.position.y, transform.position.z+ Mathf.Sign(Input.GetAxis("Vertical"))),JumpPower,1, MovementDuration, false)).SetEase(JumpEase);
+                Jump.Append(transform.DOShakeScale(0.15f, ShakePower * new Vector3(-1, 1, -1), 1, 90, true));
             }
         }
-
-
-
-            
     }
 }
