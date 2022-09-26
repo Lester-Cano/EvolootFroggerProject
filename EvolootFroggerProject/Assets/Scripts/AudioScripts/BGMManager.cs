80f6eb137daed91e62cc6b3cefa6660de1abcca3
Alonso <almear01@gmail.com>
2022-09-26 11:00:26 -0500

    Background Music

---
+++ EvolootFroggerProject/Assets/Scripts/AudioScripts/BGMManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGMManager : MonoBehaviour
+{
+   [SerializeField] private GameManager myManager;
+    private AudioSource mySource;
+    [SerializeField] AudioClip[] bgMusic;
+
+    private void Awake()
+    {
+        mySource = GetComponent<AudioSource>();
+        
+    }
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneloaded;
+        
+    }
+   
+    private void OnLosing()
+    {
+        mySource.clip = bgMusic[2];
+        mySource.Play();
+    }
+    private void OnSceneloaded(Scene scene, LoadSceneMode mode)
+    {
+        if (myManager == null)
+        {
+            myManager = FindObjectOfType<GameManager>();
+            
+        }
+        if (myManager != null)
+        {
+            myManager.OnGameOverLost += OnLosing;
+        }
+        if (scene.name == "MainMenu" && mySource != null)
+        {
+            mySource.clip = bgMusic[0];
+            mySource.Play();
+        }
+        if ((scene.name == "Testeo" || scene.name == "MainScene") && mySource != null)
+        {
+            mySource.clip = bgMusic[1];
+            mySource.Play();
+        }
+    }
+}
